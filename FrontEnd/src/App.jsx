2116a74521e3771a8dbc8c4afd48d8ef16e5b953
wayan darma <wayandarma2010@gmail.com>
2024-03-20 09:02:42 +0800

    change the App.jsx file

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,5 +1,12 @@
 import "./app.css";
+
 function App() {
+  const thisMessage = "100 days of react js";
+  let count = 0;
+  const incrementCount = () => {
+    count++;
+  };
+
   return <div className="text-9xl text-orange-400">100 days of react js</div>;
 }
 
